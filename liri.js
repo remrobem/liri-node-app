725ee78b11f90c78ba52d9ba8ff256a9d1d094d8
Rob Martin <rob.martin.5758@gmail.com>
2018-04-22 02:15:39 -0400

    handle multi word parameters

--- liri.js
+++ liri.js
@@ -43,12 +43,13 @@ switch (command) {
         badCommand();
 }
 
+//Twitter API to search user timeline for tweets. Each tweet is written to console log along with the Twitter create data/time
 function tweets() {
+
     client.get('statuses/user_timeline', twitterParams, function (error, tweets, response) {
         if (!error) {
             console.log(`Last ${tweetCount} tweets for @${user}:`)
             tweets.forEach(tweet => {
-                
                 console.log(`Tweet: ${tweet.text} | Created: ${tweet.created_at}`);
             });
         } else {
@@ -58,23 +59,55 @@ function tweets() {
 };
 
 function song() {
+    let songInfo = getSpotify(parameterString());
+};
 
+function getSpotify(songTitle) {
+   
+// spotify API goes here
+
+    let songInfo = {
+        'title': 'aa'
+    };
+    return songInfo;
 };
 
 function movie() {
-
+    let movieInfo = getOMDB(parameterString());
 };
+
+function getOMDB(movieTitle) {
+   
+    // OMDB API goes here
+    
+        let movieInfo = {
+            'title': 'aa'
+        };
+        return movieInfo;
+    };
 
 function random() {
 
 };
 
+// convert the paramaters passed after the command into a string
+function parameterString() {
+    let parameters = process.argv.slice(3, process.argv.length);
+    let parameterStr = '';
+    parameters.forEach((word) => {
+        parameterStr = parameterStr + ' ' + word;
+    });
+    // remove the 1st character from the string. Not sure why it is getting added.
+    parameterStr = parameterStr.substring(1);
+    return parameterStr;
+}
+
 function help() {
     console.log(`Valid requests are:`)
     console.log(`  my-tweets`)
-    console.log(`  spotify-this-song`)
-    console.log(`  movie-this`)
-    console.log(`  do-what-it-says`)
+    console.log(`  spotify-this-song  followed by a song title`)
+    console.log(`  movie-this   followed by a movie title`)
+    console.log(`  do-what-it-says  `)
 };
 
 function badCommand() {
